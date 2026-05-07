bffa73e1f83f5688df72cdeb17d53bb3172a7c37
carolmt <carolinamaldonado12@hotmail.es>
2026-05-07 12:41:29 +0100

    componente info añadido pero no ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./home/home.component').then(
+                (c)=> c.HomeComponent
+            ),
+    },
+    {
+        path:'info',
+        loadComponent: () =>
+            import('./info-page/info-page.component').then(
+                (c)=> c.InfoPageComponent
+            ),    }   
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { InfoPageComponent } from '../info-page/info-page.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
